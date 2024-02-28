67bf54feb073ec62ff2e6f263da24b785c997ef8
weijianhong <wei_iianhong@tianfangsec.com>
2024-02-28 16:47:38 +0800

    a

--- router/base/base.go
+++ router/base/base.go
@@ -10,7 +10,8 @@ var baseRouterNew = new(baseRouter)
 
 func (s *baseRouter) Add(rg *gin.RouterGroup) {
 
-	rg.GET("captcha", baseApi.Captcha)    // 获取验证码
+	rg.GET("captcha", baseApi.Captcha) // 获取验证码
+
 	rg.POST("login", baseApi.Login)       // 登录
 	rg.POST("register", baseApi.Register) // 注册
 
