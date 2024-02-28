2b4abe6ff52553ea72a466e0e5416eb9f21523ad
weijianhong <wei_iianhong@tianfangsec.com>
2024-02-28 16:12:25 +0800

    a

--- initialize/internal/logger.go
+++ initialize/internal/logger.go
@@ -12,13 +12,11 @@ type writer struct {
 }
 
 // NewWriter writer 构造函数
-// Author [SliverHorn](https://github.com/SliverHorn)
 func NewWriter(w logger.Writer) *writer {
 	return &writer{Writer: w}
 }
 
 // Printf 格式化打印日志
-// Author [SliverHorn](https://github.com/SliverHorn)
 func (w *writer) Printf(message string, data ...interface{}) {
 	var logZap bool
 	switch global.CONFIG.System.DbType {
